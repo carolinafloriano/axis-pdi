b6dc0de96b214edbd57b5930da5a306cd81ca6a5
Carolina Floriano <carolina.floriano@gmail.com>
2023-02-28 15:56:08 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,103 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Card class="search-container">
+    <template #title> Busca de CEP </template>
+    <template #content>
+      <div class="search-field">
+        <span class="fields">
+          <label for="cep">CEP</label>
+          <InputMask id="cep" v-model="cep" mask="99999-999" />
+          <Button label="Submit" @click="buscaCep()" />
+        </span>
+      </div>
+      <hr />
+      <div class="results">
+        <span class="fields">
+          <label for="username">Username</label>
+          <InputText id="username" type="text" v-model="value" />
+        </span>
+      </div>
+    </template>
+  </Card>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import InputMask from "primevue/inputmask";
+import InputText from "primevue/inputtext";
+import Card from "primevue/card";
+import Button from "primevue/button";
+
+const axios = require("axios");
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { InputMask, InputText, Card, Button },
+  data() {
+    return {
+      cep: "",
+    };
+  },
+
+  methods: {
+    buscaCep() {
+      axios
+        .get("https://viacep.com.br/ws/" + this.cep + "/json/")
+        .then((response) => {
+          console.log(response.data);
+        });
+    },
   },
 };
 </script>
 
 <style lang="scss">
 #app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  > .p-card {
+    background: #fbfafa;
+  }
+}
+
+.search-container {
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  padding: 20px;
+
+  > .p-card-body {
+    > .p-card-content {
+      > .search-field {
+        display: flex;
+        justify-content: space-between;
+
+        > .fields {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+        }
+      }
+
+      > .results {
+        display: flex;
+        justify-content: space-between;
+
+        > .fields {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+        }
+      }
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+//PrimeVue Config
+import PrimeVue from "primevue/config";
+
+//PrimeVue CSS
+import "primevue/resources/themes/lara-light-purple/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+const app = createApp(App);
+app.use(PrimeVue);
+
 createApp(App).mount("#app");
